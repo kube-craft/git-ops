03978a16f06ed109eb7f079043b417c3c26a9315
Vikrant Verma <vikrant.verma2002@gmail.com>
2024-08-19 15:49:31 +0400

    argo cd

--- argocd-declarative/run.sh
+++ argocd-declarative/run.sh
@@ -1,11 +1,18 @@
 #Start single apps
-k -n argocd apply -f argocd-declarative/argocd-apps/argocd-app.yml
-k -n argocd apply -f https://github.com/kube-craft/git-ops/blob/main/argocd-declarative/argocd-apps/argocd-app.yml
+#geocentric
+k -n argocd apply -f argocd-declarative/argocd-apps/geocentric-app.yml
+k -n argocd apply -f https://raw.githubusercontent.com/kube-craft/git-ops/main/argocd-declarative/argocd-apps/geocentric-app.yml
+
+#argocd
+k -n argocd apply -f argocd-declarative/argocd-apps/argo-app.yml
+k -n argocd apply -f https://raw.githubusercontent.com/kube-craft/git-ops/main/argocd-declarative/argocd-apps/argo-app.yml
+
+
 
 
 #Start all apps
 k -n argocd apply -f argocd-declarative/argocd-multi-apps/argocd-multi-apps.yml
-k -n argocd apply -f https://github.com/kube-craft/git-ops/blob/main/argocd-declarative/argocd-multi-apps/argocd-multi-apps.yml
+k -n argocd apply -f https://raw.githubusercontent.com/kube-craft/git-ops/main/argocd-declarative/argocd-multi-apps/argocd-multi-apps.yml
 
 
 k -n argocd get applications
