beb9a06b61ef560c69ec6886952a6a568c3a2330
Vikrant Verma <vikrant.verma2002@gmail.com>
2024-08-20 12:01:54 +0400

    Update run.sh

--- argocd-declarative/run.sh
+++ argocd-declarative/run.sh
@@ -8,15 +8,18 @@ k -n argocd apply -f https://raw.githubusercontent.com/kube-craft/git-ops/main/a
 k -n argocd apply -f https://raw.githubusercontent.com/kube-craft/git-ops/main/argocd-declarative/manifests/argocd-app/private-gitea-repo.yml
 
 # Add Projects
-#geocentric
-k -n argocd apply -f argocd-declarative/argocd-apps/geocentric-app.yml
-k -n argocd apply -f https://raw.githubusercontent.com/kube-craft/git-ops/main/argocd-declarative/argocd-apps/geocentric-app.yml
 
 #argocd
 k -n argocd apply -f argocd-declarative/argocd-apps/argocd-app.yml
 k -n argocd apply -f https://raw.githubusercontent.com/kube-craft/git-ops/main/argocd-declarative/argocd-apps/argocd-app.yml
 
+#geocentric
+k -n argocd apply -f argocd-declarative/argocd-apps/geocentric-app.yml
+k -n argocd apply -f https://raw.githubusercontent.com/kube-craft/git-ops/main/argocd-declarative/argocd-apps/geocentric-app.yml
 
+#gitea
+k -n argocd apply -f argocd-declarative/argocd-apps/gitea-app.yml
+k -n argocd apply -f https://raw.githubusercontent.com/kube-craft/git-ops/main/argocd-declarative/argocd-apps/gitea-app.yml
 
 
 #Start all apps
@@ -25,3 +28,6 @@ k -n argocd apply -f https://raw.githubusercontent.com/kube-craft/git-ops/main/a
 
 
 k -n argocd get applications
+
+#get initial password
+k -n argocd get secrets argocd-initial-admin-secret -o json | jq .data.password -r | tr -d '\n'  | base64 -d
