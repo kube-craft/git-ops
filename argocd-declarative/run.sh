74be6cf5487031163e064b206cfe77c288bdce86
Vikrant Verma <vikrant.verma2002@gmail.com>
2024-08-20 08:52:42 +0400

    argo repo added

--- argocd-declarative/run.sh
+++ argocd-declarative/run.sh
@@ -1,11 +1,20 @@
 #Start single apps
+# Install Argo
+k create namespace argocd
+k -n argocd apply -f https://raw.githubusercontent.com/kube-craft/git-ops/main/argocd-declarative/manifests/argocd-app/argocd-install.yml
+
+# Add repo
+k -n argocd apply -f https://raw.githubusercontent.com/kube-craft/git-ops/main/argocd-declarative/manifests/argocd-app/public-github-repo.yml
+k -n argocd apply -f https://raw.githubusercontent.com/kube-craft/git-ops/main/argocd-declarative/manifests/argocd-app/private-gitea-repo.yml
+
+# Add Projects
 #geocentric
 k -n argocd apply -f argocd-declarative/argocd-apps/geocentric-app.yml
 k -n argocd apply -f https://raw.githubusercontent.com/kube-craft/git-ops/main/argocd-declarative/argocd-apps/geocentric-app.yml
 
 #argocd
-k -n argocd apply -f argocd-declarative/argocd-apps/argo-app.yml
-k -n argocd apply -f https://raw.githubusercontent.com/kube-craft/git-ops/main/argocd-declarative/argocd-apps/argo-app.yml
+k -n argocd apply -f argocd-declarative/argocd-apps/argocd-app.yml
+k -n argocd apply -f https://raw.githubusercontent.com/kube-craft/git-ops/main/argocd-declarative/argocd-apps/argocd-app.yml
 
 
 
