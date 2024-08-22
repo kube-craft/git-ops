14b1bfe64c7a1cd96f77296dc2afa7bf90286e72
Vikrant Verma <vikrant.verma2002@gmail.com>
2024-08-22 15:43:32 +0400

    projects created

--- argocd-declarative/run.sh
+++ argocd-declarative/run.sh
@@ -1,3 +1,6 @@
+# prerequisites for new cluster
+k apply -f https://raw.githubusercontent.com/kube-craft/git-ops/main/argocd-declarative/manifests/ingress-app/baremetal-deploy.yml
+
 #Start single apps
 # Install Argo
 k create namespace argocd
@@ -7,11 +10,21 @@ k -n argocd apply -f https://raw.githubusercontent.com/kube-craft/git-ops/main/a
 k -n argocd apply -f https://raw.githubusercontent.com/kube-craft/git-ops/main/argocd-declarative/manifests/argocd-app/public-github-repo.yml
 k -n argocd apply -f https://raw.githubusercontent.com/kube-craft/git-ops/main/argocd-declarative/manifests/argocd-app/private-gitea-repo.yml
 
-# Add Projects
+# Add Projects and Applications
+k -n argocd apply -f https://raw.githubusercontent.com/kube-craft/git-ops/main/argocd-declarative/manifests/argocd-app/projects/infrastructure.yml
 
 #argocd
 k -n argocd apply -f argocd-declarative/argocd-apps/argocd-app.yml
 k -n argocd apply -f https://raw.githubusercontent.com/kube-craft/git-ops/main/argocd-declarative/argocd-apps/argocd-app.yml
+
+k -n argocd get applications
+
+#get initial password
+k -n argocd get secrets argocd-initial-admin-secret -o json | jq .data.password -r | tr -d '\n'  | base64 -d
+
+
+
+
 
 #geocentric
 k -n argocd apply -f argocd-declarative/argocd-apps/geocentric-app.yml
@@ -22,12 +35,6 @@ k -n argocd apply -f argocd-declarative/argocd-apps/gitea-app.yml
 k -n argocd apply -f https://raw.githubusercontent.com/kube-craft/git-ops/main/argocd-declarative/argocd-apps/gitea-app.yml
 
 
-#Start all apps
+#Start all apps (self heal is off and manual sync is enabled)
 k -n argocd apply -f argocd-declarative/argocd-multi-apps/argocd-multi-apps.yml
 k -n argocd apply -f https://raw.githubusercontent.com/kube-craft/git-ops/main/argocd-declarative/argocd-multi-apps/argocd-multi-apps.yml
-
-
-k -n argocd get applications
-
-#get initial password
-k -n argocd get secrets argocd-initial-admin-secret -o json | jq .data.password -r | tr -d '\n'  | base64 -d
