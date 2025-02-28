d3a3d5b18da84a8cb07be731758bea09c7b766b4
Vikrant Verma <vikrant.verma2002@gmail.com>
2025-02-28 18:09:20 +0400

    long horn added

--- argocd-declarative/run.sh
+++ argocd-declarative/run.sh
@@ -34,7 +34,7 @@ k -n argocd apply -f https://raw.githubusercontent.com/kube-craft/git-ops/main/a
 k -n argocd apply -f https://raw.githubusercontent.com/kube-craft/git-ops/main/argocd-declarative/argocd-apps/metrics-app.yml
 k -n argocd apply -f https://raw.githubusercontent.com/kube-craft/git-ops/main/argocd-declarative/argocd-apps/reloader-app.yml
 
-
+k -n argocd apply -f https://raw.githubusercontent.com/kube-craft/git-ops/main/argocd-declarative/argocd-apps/longhorn-app.yml
 k -n argocd apply -f https://raw.githubusercontent.com/kube-craft/git-ops/main/argocd-declarative/argocd-apps/nifi-app.yml
 
 
